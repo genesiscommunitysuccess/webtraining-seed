3e5bb9d4cee9344231b05c3b799c88650b607f96
GuiWar <guilherme.guerra@genesis.global>
2023-05-04 20:46:57 +0000

    Exercise 1.3

--- client/web/src/layouts/default.ts
+++ client/web/src/layouts/default.ts
@@ -51,59 +51,30 @@ export const defaultLayout = new FASTElementLayout(
             </zero-button>
           `
         )}
-        <span slot="group-title-1">GROUP SLOT</span>
-        <zero-tree-view slot="nav-items-1">
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item
-          </zero-tree-item>
-        </zero-tree-view>
-        <span slot="group-title-2">GROUP SLOT 2</span>
-        <zero-tree-view slot="nav-items-2">
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item 2
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item 2
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item 2
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Slot Tree Item 2
-          </zero-tree-item>
-        </zero-tree-view>
-        <span slot="group-title-3">GROUP SLOT 3</span>
-        <zero-tree-view slot="nav-items-3">
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            User Slot
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Reporting Slot
-          </zero-tree-item>
-          <zero-tree-item>
-            <zero-icon variant="solid" name="location-arrow"></zero-icon>
-            Settings Slot
-          </zero-tree-item>
-        </zero-tree-view>
+        <div slot="menu-contents">
+            <p>GROUP SLOT</p>
+            <zero-tree-view>
+              <zero-tree-item>
+                <zero-icon variant="solid" name="location-arrow"></zero-icon>
+                <zero-anchor @click=${(x) => x.navigation.navigateTo("/playground")}> Playground</zero-anchor>
+              </zero-tree-item>
+              <zero-tree-item>
+                <zero-icon variant="solid" name="location-arrow"></zero-icon>
+                Slot Tree Item
+              </zero-tree-item>
+            </zero-tree-view>
+            <p>GROUP SLOT 2</p>
+            <zero-tree-view>
+              <zero-tree-item>
+                <zero-icon variant="solid" name="location-arrow"></zero-icon>
+                Slot Tree Item 2
+              </zero-tree-item>
+              <zero-tree-item>
+                <zero-icon variant="solid" name="location-arrow"></zero-icon>
+                Slot Tree Item 2
+              </zero-tree-item>
+            </zero-tree-view>
+        </div>
       </foundation-header>
       <div class="content">
         <slot></slot>
