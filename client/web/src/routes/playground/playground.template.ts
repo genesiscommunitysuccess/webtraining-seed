fb9fa66c51d982b9210ef08f675bf92fdc3b085c
Guilherme <guilherme.guerra@genesis.global>
2023-11-06 12:53:15 -0300

    foundation layout

--- client/web/src/routes/playground/playground.template.ts
+++ client/web/src/routes/playground/playground.template.ts
@@ -2,10 +2,36 @@ import {html, ref, repeat, when} from '@microsoft/fast-element';
 import {Playground} from './playground';
 import {playgroundStyles} from './playground.styles';
 import {sync} from '@genesislcap/foundation-utils'
-import {DEFAULT_LAYOUT} from './pre-defined-layout';
 
 
 export const myTemplate = html<Playground>`
     <h2>This is my first component</h2>
+    <zero-button @click=${(x) => x.resetLayout()}>Reset</zero-button>
+    <zero-layout ${ref('layout')} auto-save-key="training-layout-key">
+        <zero-layout-region>
+            <zero-layout-region type="horizontal">
+                <zero-layout-region type="vertical">
+                    <zero-layout-item title="Region 1">
+                    </zero-layout-item>
+                    <zero-layout-item title="Region 2">
+                    </zero-layout-item>
+                    <zero-layout-item title="Region 3">
+                    </zero-layout-item>
+                    <zero-layout-item title="Region 4">
+                    </zero-layout-item>
+                    <zero-layout-item title="Region 5">
+                    </zero-layout-item>
+                </zero-layout-region>
+                <zero-layout-region type="vertical">
+                    <zero-layout-item title="Region 6">
+                    </zero-layout-item>
+                </zero-layout-region>
+                <zero-layout-region type="vertical">
+                    <zero-layout-item title="Region 7">
+                    </zero-layout-item>
+                </zero-layout-region>
+            </zero-layout-region>
+        </zero-layout-region>
+    </zero-layout>
 
 `;
--- client/web/src/routes/playground/playground.ts
+++ client/web/src/routes/playground/playground.ts
@@ -1,16 +1,12 @@
 import {customElement, FASTElement, observable} from '@microsoft/fast-element';
 import {myTemplate as template} from './playground.template';
 import {playgroundStyles as styles} from './playground.styles';
-import {Connect} from '@genesislcap/foundation-comms';
-import {Modal} from '@genesislcap/foundation-zero'
-import {GridPro} from '@genesislcap/grid-pro';
-import {Form} from '@genesislcap/foundation-forms';
-import {FoundationLayout, CustomButton, LAYOUT_ICONS, LayoutReceiveEvents, LayoutReceiveEventsDetail } from '@genesislcap/foundation-layout'
+import {FoundationLayout} from '@genesislcap/foundation-layout'
 import {DEFAULT_LAYOUT} from './pre-defined-layout';
+
 
 const name = 'playground-route'
 
-Form
 
 @customElement({
     name,
@@ -19,101 +15,9 @@ Form
 })
 
 export class Playground extends FASTElement {
+    layout: FoundationLayout
 
-    @Connect connection: Connect
-    @observable resourceName: string
-    @observable columnsDefTrade = [
-        {field: "TRADE_ID", onCellDoubleClicked: (e) => this.openModifyModal(e.data)},
-        {field: "DIRECTION"},
-        {field: "COUNTERPARTY_ID"},
-        {field: "INSTRUMENT_ID"},
-        {field: "QUANTITY"},
-        {field: "PRICE"},
-        {field: "SYMBOL"},
-        {field: "TRADE_STATUS"},
-    ]
-
-  @observable chartConfiguration = {
-    width: 800,
-    angleField: 'value',
-    colorField: 'groupBy',
-    radius: 0.75,
-    label: {
-      type: 'spider',
-      labelHeight: 28,
-      content: '{name}\n{percentage}',
-      style: {
-        fill: 'white',
-      },
-    },
-    interactions: [{ type: 'element-selected' }, { type: 'element-active' }],
-  };
-
-    @observable data: {
-        COUNTERPARTY_ID: string;
-        DIRECTION: string;
-        ENTERED_BY: string;
-        INSTRUMENT_ID: string;
-        PRICE: number;
-        QUANTITY: number;
-        SYMBOL: string;
-        TRADE_ID: string;
-        TRADE_STATUS: string;
-    }
-    @observable buttonDefinition: CustomButton[] = [ {
-        svg: LAYOUT_ICONS.renameSVG,
-        onClick: (button: HTMLElement, elem: HTMLElement) => {
-          const title = prompt('New name?');
-          const event: LayoutReceiveEventsDetail['changeTitle'] = {
-            title,
-            mode: 'replace',
-          };
-          elem.dispatchEvent(
-            new CustomEvent(LayoutReceiveEvents.changeTitle, { detail: event, bubbles: true }),
-          );
-        },
+      resetLayout() {
+        this.layout.loadLayout(JSON.parse(DEFAULT_LAYOUT));
       }
-    ]
-
-  resetLayout() {
-    this.layout.loadLayout(JSON.parse(DEFAULT_LAYOUT));
-  }
-
-    tradeGrid: GridPro
-    tradeForms: Form
-    trade_id: string;
-    newTradeModal: Modal
-    layout: FoundationLayout;
-
-    closeModal(){
-        this.newTradeModal.close()
-        this.tradeForms.reset()
-    }
-
-    openModifyModal(rowData){
-        this.resourceName = "EVENT_TRADE_MODIFY"
-        this.data ={
-            COUNTERPARTY_ID: rowData.COUNTERPARTY_ID,
-            DIRECTION: rowData.DIRECTION,
-            ENTERED_BY: rowData.ENTERED_BY,
-            INSTRUMENT_ID: rowData.INSTRUMENT_ID,
-            PRICE: rowData.PRICE,
-            QUANTITY: rowData.QUANTITY,
-            SYMBOL: rowData.SYMBOL,
-            TRADE_ID: rowData.TRADE_ID,
-            TRADE_STATUS: rowData.TRADE_STATUS,
-        }
-        this.newTradeModal.show()
-    }
-
-    createInsertTradeForms(){
-        this.resourceName = "EVENT_TRADE_INSERT"
-        this.newTradeModal.show()
-    }
-
-    async connectedCallback() {
-        super.connectedCallback();
-
-    }
-
 }
