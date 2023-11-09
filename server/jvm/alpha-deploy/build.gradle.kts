33d0733dbff985bdd67715e793d808a8653c382b
dbarros1979 <daniel.barros@genesis.global>
2023-11-09 19:39:16 +0000

    Adding GSF7 and FUI 14.101.1

--- server/jvm/alpha-deploy/build.gradle.kts
+++ server/jvm/alpha-deploy/build.gradle.kts
@@ -7,7 +7,7 @@ description = "alpha-deploy"
 dependencies {
     api(project(":alpha-eventhandler"))
     api(project(":alpha-messages"))
-    
+
     /* dependencies required for building the docker image from the Gradle task */
     genesisServer(
         group = "global.genesis",
@@ -28,29 +28,10 @@ dependencies {
     genesisWeb("client:web")
     /* --- */
 
-    /* dependencies required for building the docker image from local docker compose */
-    implementation(
-        group = "global.genesis",
-        name = "genesis-distribution",
-        version = properties["genesisVersion"].toString(),
-        classifier = "bin",
-        ext = "zip"
-    )
-    implementation(
-        group = "global.genesis",
-        name = "auth-distribution",
-        version = properties["authVersion"].toString(),
-        classifier = "bin",
-        ext = "zip"
-    )
-    /* --- */
-
     /* Add additional dependencies on other external distributions here */
-    implementation(
-        group = "global.genesis",
-        name = "reporting-distribution",
-        version = properties["reportingVersion"].toString(),
-        classifier = "bin",
-        ext = "zip"
-    )
+}
+tasks {
+    copyDependencies {
+        from(configurations.getByName("genesisServer"))
+    }
 }
